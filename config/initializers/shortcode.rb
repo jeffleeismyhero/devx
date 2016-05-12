e7d39e68735da6176ab1b8c6ce4a86880bdc1cb7
Rashaad R. Randall <rrandall@vividitc.com>
2016-05-11 21:07:59 -0500

    Shortcode fix

--- config/initializers/shortcode.rb
+++ config/initializers/shortcode.rb
@@ -1,7 +1,7 @@
 Shortcode.setup do |config|
   config.template_path = "#{Devx::Engine.root}/app/views/devx/shortcode_templates"
   config.block_tags = [ ]
-  config.self_closing_tags = [ :slideshow, :event_list_boxed, :media, :latest_article ]
+  config.self_closing_tags = [ :slideshow, :event_list_boxed, :media, :latest_articles ]
 end
 
 Shortcode.register_presenter(Devx::SlideshowPresenter, Devx::EventListBoxedPresenter, Devx::MediaPresenter, Devx::LatestArticlesPresenter)
