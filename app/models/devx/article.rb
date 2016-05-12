e02435870312053ad6b8107657e9c94c4b10a624
Rashaad R. Randall <rrandall@vividitc.com>
2016-05-11 21:04:02 -0500

    Modifies article model

--- app/models/devx/article.rb
+++ app/models/devx/article.rb
@@ -1,5 +1,7 @@
 module Devx
   class Article < ActiveRecord::Base
+    scope :latest, -> { order(created_at: :desc) }
+
     validates :title, presence: true
     validates :content, presence: true
   end
