a4cc216792809a95dd159c23b4f46fd8af6186af
Rashaad Randall <rashaad@jcwproductions.com>
2016-05-17 11:29:35 -0500

    Adds CKEditor precompile

--- lib/devx/engine.rb
+++ lib/devx/engine.rb
@@ -1,5 +1,11 @@
 module Devx
   class Engine < ::Rails::Engine
     isolate_namespace Devx
+
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+
+    initializer 'devx.assets.precompile' do |app|
+      app.config.assets.precompile += app.config.assets.precompile += %w( ckeditor/* )
+    end
   end
 end
