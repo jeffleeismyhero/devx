8974cf3da1734baae73a6e16036674c89e1d7a48
Rashaad R. Randall <rrandall@vividitc.com>
2016-05-25 14:37:51 -0500

    Remove requires for inactive gems

--- lib/devx.rb
+++ lib/devx.rb
@@ -1,6 +1,5 @@
 require "devx/engine"
 require "devx/version"
-require "acts-as-taggable-on"
 
 Gem.loaded_specs['devx'].dependencies.each do |d|
  require d.name
--- lib/devx/engine.rb
+++ lib/devx/engine.rb
@@ -2,9 +2,6 @@ module Devx
   class Engine < ::Rails::Engine
     isolate_namespace Devx
 
-    require "acts-as-taggable-on"
-
-
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
     initializer 'devx.assets.precompile' do |app|
