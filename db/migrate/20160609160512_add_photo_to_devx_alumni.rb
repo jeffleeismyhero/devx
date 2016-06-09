0395a5be48382f38eb9a97251cd4a47d5457cce4
kellyhart <kelly@jcwproductions.com>
2016-06-09 14:58:21 -0500

    more forms

---
+++ db/migrate/20160609160512_add_photo_to_devx_alumni.rb
@@ -0,0 +1,5 @@
+class AddPhotoToDevxAlumni < ActiveRecord::Migration
+  def change
+    add_column :devx_alumnis, :photo, :string
+  end
+end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609143722) do
+ActiveRecord::Schema.define(version: 20160609160512) do
 
   create_table "devx_alumnis", force: :cascade do |t|
     t.integer  "user_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160609143722) do
     t.integer  "graduation_year"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "photo"
   end
 
   create_table "devx_articles", force: :cascade do |t|
@@ -131,8 +132,6 @@ ActiveRecord::Schema.define(version: 20160609143722) do
     t.datetime "updated_at",    null: false
     t.integer  "calendar_id"
   end
-
-  add_index "devx_events", ["calendar_id"], name: "index_devx_events_on_calendar_id"
 
   create_table "devx_extracurriculars", force: :cascade do |t|
     t.string   "name"
