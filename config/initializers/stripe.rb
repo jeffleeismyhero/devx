49a3ef80f5f2a2833b376fab4244bc5dec812d52
Rashaad R. Randall <rrandall@vividitc.com>
2016-06-13 16:30:08 -0500

    fix

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
-Rails.configuration.stripe = {
-  :publishable_key => Devx::ApplicationSetting.find(1).settings['stripe_publishable_key'],
-  :secret_key      => Devx::ApplicationSetting.find(1).settings['stripe_secret_key']
-}
+# Rails.configuration.stripe = {
+#   :publishable_key => Devx::ApplicationSetting.find(1).settings['stripe_publishable_key'],
+#   :secret_key      => Devx::ApplicationSetting.find(1).settings['stripe_secret_key']
+# }
 
-Stripe.api_key = Devx::ApplicationSetting.find(1).settings['stripe_secret_key']
+# Stripe.api_key = Devx::ApplicationSetting.find(1).settings['stripe_secret_key']
