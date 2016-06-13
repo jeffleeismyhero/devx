0b528dee9dbf3a25da67b9b2283bcd39ad97cdc8
Rashaad R. Randall <rrandall@vividitc.com>
2016-06-13 17:21:22 -0500

    bug fix

--- db/migrate/20160610093248_add_person_to_devx_users.rb
+++ db/migrate/20160610093248_add_person_to_devx_users.rb
@@ -1,5 +1,6 @@
 class AddPersonToDevxUsers < ActiveRecord::Migration
   def change
-    add_reference :devx_users, :person, index: true, foreign_key: true
+    add_reference :devx_users, :person_id, :integer
+    add_index :devx_users, :person_id
   end
 end
