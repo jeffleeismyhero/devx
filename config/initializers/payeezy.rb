306a2a64cf74fcfef0a3f635f4865dbc7227d6f4
Rashaad R. Randall <rrandall@vividitc.com>
2016-06-13 16:40:50 -0500

    Updates

--- config/initializers/payeezy.rb
+++ config/initializers/payeezy.rb
@@ -4,9 +4,9 @@ module PayeezyTransaction
   def self.process(amount, cc_type, ch_name, cc_number, exp_date, cvv)
     transaction = Payeezy::Transactions.new(
       url: 'https://api-cert.payeezy.com/v1/transactions',
-      apikey: Devx::ApplicationSetting.find(1).settings['payeezy_api_key'],
-      apisecret: Devx::ApplicationSetting.find(1).settings['payeezy_api_secret'],
-      token: Devx::ApplicationSetting.find(1).settings['payeezy_api_token']
+      apikey: Devx::ApplicationSetting.find_or_create_by(id: 1).settings['payeezy_api_key'],
+      apisecret: Devx::ApplicationSetting.find_or_create_by(id: 1).settings['payeezy_api_secret'],
+      token: Devx::ApplicationSetting.find_or_create_by(id: 1).settings['payeezy_api_token']
     ) rescue nil
 
     params = {
