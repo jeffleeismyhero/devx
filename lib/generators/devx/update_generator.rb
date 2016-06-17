4fb468e0c30b21e0aa78e8e179b9df0f4221b63e
Rashaad Randall <rashaad@jcwproductions.com>
2016-06-17 14:16:19 -0500

    Updates generator to only reflect updates specific to the engine

--- lib/generators/devx/update_generator.rb
+++ lib/generators/devx/update_generator.rb
@@ -1,7 +1,7 @@
 module Devx
   class UpdateGenerator < Rails::Generators::Base
     def update
-      run 'bundle update devx'
+      run 'bundle update devx | grep devx'
       rake 'db:migrate'
     end
   end
