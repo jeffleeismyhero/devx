15df8ee8cf1cf4d60b00555a25c6df15fe1a64f3
Rashaad R. Randall <rrandall@vividitc.com>
2016-07-01 11:20:38 -0500

    CKEditor fix

--- app/assets/javascripts/devx/ckeditor/config.js
+++ app/assets/javascripts/devx/ckeditor/config.js
@@ -1,3 +1,5 @@
 CKEDITOR.config.enterMode = CKEDITOR.ENTER_BR;
 CKEDITOR.config.entities = false;
 CKEDITOR.config.allowedContent = true
+CKEDITOR.config.protectedSource.push(/<i[^>]*><\/i>/g);
+CKEDITOR.dtd.$removeEmpty['i'] = false
