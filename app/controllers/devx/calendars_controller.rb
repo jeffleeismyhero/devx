4c88d75a0b88e61a9511f8a4c756a628e0ca15eb
Rashaad R. Randall <rrandall@vividitc.com>
2016-07-02 23:27:30 -0500

    calendar fix

--- app/controllers/devx/calendars_controller.rb
+++ app/controllers/devx/calendars_controller.rb
@@ -24,7 +24,7 @@ module Devx
 
 
       if @calendar.calendar_type == 'Standard'
-        @events = @calendar.events.for(Time.now, Time.now)
+        @events = @calendar.events.for(Time.now, Time.now).upcoming
 
         @dates = []
         @events.try(:each) do |event|
