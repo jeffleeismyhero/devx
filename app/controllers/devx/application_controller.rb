dda2a66a2620151b1bf6909f4a0c5f46f489af23
Jeffrey Lee <jlee42@gmail.com>
2016-08-04 16:06:19 -0500

    Set beginning of week to Sunday within DevX
    - Add week calendar view with working previous and next links

--- app/controllers/devx/application_controller.rb
+++ app/controllers/devx/application_controller.rb
@@ -1,10 +1,11 @@
 module Devx
   class ApplicationController < ActionController::Base
+    Date.beginning_of_week = :sunday
 
     def app_settings
       Devx::ApplicationSetting.find_or_create_by(id: 1).settings
     end
-    
+
     def app_branding
       Devx::Branding.find_or_create_by(id: 1)
     end
