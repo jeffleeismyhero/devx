7ad304da4e7efc821b2c440f83a7a2f5f16a0b5d
Rashaad R. Randall <rrandall@vividitc.com>
2016-08-08 10:33:53 -0500

    Event validation fix

--- app/models/devx/event.rb
+++ app/models/devx/event.rb
@@ -14,7 +14,7 @@ module Devx
 
     validates :name, presence: true
     validates :schedules, presence: true
-    validates :google_event_id, uniqueness: true
+    validates :google_event_id, uniqueness: true, allow_nil: true
     validate :check_for_duplicates
 
     accepts_nested_attributes_for :schedules, allow_destroy: true,
