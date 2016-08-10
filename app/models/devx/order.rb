114c2aa42df6f5dba03a50fc27ee6558adad9055
Jeffrey Lee <jlee42@gmail.com>
2016-08-10 12:14:02 -0500

    Clean up order totaling/calculation methods

--- app/models/devx/order.rb
+++ app/models/devx/order.rb
@@ -6,31 +6,21 @@ module Devx
 
     validates :user_id, presence: true
 
-
     accepts_nested_attributes_for :line_items, allow_destroy: true,
       reject_if: proc{ |x| x['quantity'] == 0 }
     accepts_nested_attributes_for :transactions, allow_destroy: true,
       reject_if: proc{ |x| x['amount'].blank? }
 
     def total
-      total = 0
+      line_items.to_a.sum(&:line_total)
+    end
 
-      self.line_items.try(:each) do |line_item|
-        total += (line_item.product.price * line_item.quantity)
-      end
-
-      return total
+    def payments
+      transactions.sum(:amount) rescue 0
     end
 
     def balance
-      payments = 0
-
-      self.transactions.try(:each) do |transaction|
-        payments += transaction.amount
-      end
-
-      return (self.total - payments)
+      total - payments
     end
-
   end
 end
