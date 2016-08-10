5ead9dc601941e1c33f8f449607362299d0abb26
Jeffrey Lee <jlee42@gmail.com>
2016-08-10 13:03:02 -0500

    Add Customer Token to User model

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810174521) do
+ActiveRecord::Schema.define(version: 20160810175343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -688,6 +688,7 @@ ActiveRecord::Schema.define(version: 20160810174521) do
     t.datetime "updated_at",                          null: false
     t.string   "photo"
     t.integer  "person_id"
+    t.string   "customer_token"
   end
 
   add_index "devx_users", ["deleted_at"], name: "index_devx_users_on_deleted_at", using: :btree
