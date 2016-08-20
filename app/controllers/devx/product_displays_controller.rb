67ad86f17cf8fb72f8119c83c83e47488f6a267f
Rashaad R. Randall <rashaad@jcwproductions.com>
2016-08-19 19:00:04 -0500

    E-commerce updates

--- app/controllers/devx/product_displays_controller.rb
+++ app/controllers/devx/product_displays_controller.rb
@@ -3,18 +3,42 @@ require_dependency "devx/application_controller"
 module Devx
   class ProductDisplaysController < ApplicationController
   	before_action :set_product, only: [:show]
-  	
+    load_and_authorize_resource :product, class: 'Devx::Product'
+    layout :set_layout
+
   	def index
-  		@products = Product.all
+      if app_settings['commerce_layout'].present?
+        @layout = Devx::Layout.find(app_settings['commerce_layout'])
+      else
+        @layout = nil
+      end
+
+      @page = Devx::Page.new(name: 'Shop', layout: @layout)
+
+      @products = Devx::Product.all
   	end
 
   	def show
+      if app_settings['commerce_layout'].present?
+        @layout = Devx::Layout.find(app_settings['commerce_layout'])
+      else
+        @layout = nil
+      end
+
+      @page = Devx::Page.new(name: @product.name, layout: @layout)
   	end
 
   	private
-
-  	def set_product
+    def set_product
     	@product = Product.find(params[:id])
   	end
+
+    def set_layout
+      if app_settings['commerce_layout'].present?
+        'devx/custom'
+      else
+        'devx/application'
+      end
+    end
   end
 end
