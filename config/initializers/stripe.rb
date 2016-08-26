07d0a91795e4b68a60438c1304ee49fdfc3d2aff
David McGirt <david@jcwproductions.com>
2016-08-26 16:58:51 -0500

    Fix attempt
    #initializers may be trying to access the database before tables are
    created

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,11 +1,11 @@
-Rails.configuration.stripe = {
-  #:publishable_key => ENV['TEST_PUBLISHABLE_KEY'],
-  #:secret_key      => ENV['TEST_SECRET_KEY']
+# Rails.configuration.stripe = {
+#   #:publishable_key => ENV['TEST_PUBLISHABLE_KEY'],
+#   #:secret_key      => ENV['TEST_SECRET_KEY']
 
-  :publishable_key => Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_publishable_key'],
-  :secret_key      => Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_secret_key']
-}
+#   :publishable_key => Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_publishable_key'],
+#   :secret_key      => Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_secret_key']
+# }
 
-#Stripe.api_key = Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_secret_key']
+# #Stripe.api_key = Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_secret_key']
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
