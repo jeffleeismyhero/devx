1237eebd1d27e1cb50284dcca0619babe11acf71
Jeffrey Lee <jlee42@gmail.com>
2016-08-26 21:02:21 -0500

    Concat inner content tags

--- app/helpers/devx/application_helper.rb
+++ app/helpers/devx/application_helper.rb
@@ -97,8 +97,8 @@ module Devx
             content_tag :div, class: 'large-12 columns' do
               messages.collect do |message|
                 content_tag :div, class: 'message' do
-                  content_tag :h2, message.title, class: 'alert-title'
-                  content_tag :p, message.message, class: 'alert-message'
+                  concat content_tag :h2, message.title, class: 'alert-title'
+                  concat content_tag :p, message.message, class: 'alert-message'
                 end
               end.reduce(:<<)
             end
