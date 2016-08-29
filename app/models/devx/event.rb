ae9fc4fe2bc68aaa23ad4a9b92799c34c804aabc
Jeffrey Lee <jlee42@gmail.com>
2016-08-29 14:21:48 -0500

    Use lambda for current time based scopes

--- app/models/devx/event.rb
+++ app/models/devx/event.rb
@@ -8,7 +8,7 @@ module Devx
     belongs_to :calendar
     belongs_to :venue
 
-    scope :upcoming, -> { where("start_time > ?", Time.zone.now).order(start_time: :asc) }
+    scope :upcoming, lambda { where("start_time > ?", Time.zone.now).order(start_time: :asc) }
     scope :for, -> (start_time, end_time) { where('start_time > ? AND start_time < ?', start_time.beginning_of_month, end_time.end_of_month) }
     scope :uniq_dates, -> { uniq.pluck(:start_time) }
 
