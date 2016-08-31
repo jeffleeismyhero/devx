0f76e7d2e0d07a65a766ad52f64408dcf902718e
David McGirt <david@jcwproductions.com>
2016-08-31 13:24:49 -0500

    Rescue failures in Stripe Configurator

--- app/models/devx/stripe_configurator.rb
+++ app/models/devx/stripe_configurator.rb
@@ -1,8 +1,8 @@
 module Devx
   class StripeConfigurator
     def initialize
-      publishable_key = Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_publishable_key']
-      stripe_secret_key = Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_secret_key']
+      publishable_key = Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_publishable_key'] rescue nil
+      stripe_secret_key = Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_secret_key'] rescue nil
 
       if publishable_key.present? && stripe_secret_key.present?
         Rails.configuration.stripe = {
