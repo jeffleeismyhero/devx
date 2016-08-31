0c38dfe93ef2727df92437b6c76f56cc8c443e23
Jeffrey Lee <jlee42@gmail.com>
2016-08-31 10:34:51 -0500

    Formatting cleanup

--- app/models/devx/schedule.rb
+++ app/models/devx/schedule.rb
@@ -8,7 +8,6 @@ module Devx
 
     scope :upcoming, -> { where('(start_time < ? OR start_time >= ?) AND end_time >= ?', Time.zone.now, Time.zone.now, Time.zone.now).ordered }
     scope :coming_up, -> { where('start_time >= ?', Time.zone.now).ordered }
-    # scope :upcoming, -> { where('(start_time >= ? OR start_time <= ?) AND end_time >= ?', Time.zone.now,Time.zone.now,Time.zone.now).order(start_time: :asc) }
     scope :for_month, -> (start_time) { where('start_time >= ? AND start_time <= ?', start_time.beginning_of_month, start_time.end_of_month).ordered }
     scope :ordered, -> { order("start_time ASC, end_time ASC") }
 
