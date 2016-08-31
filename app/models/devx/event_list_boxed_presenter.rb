e5e2899e4b7d70822077d9339598b4bfef74814c
Jeffrey Lee <jlee42@gmail.com>
2016-08-31 10:35:07 -0500

    Resequence schedules for Upcoming Events list

--- app/models/devx/event_list_boxed_presenter.rb
+++ app/models/devx/event_list_boxed_presenter.rb
@@ -29,12 +29,45 @@ module Devx
     end
 
     def get_events
-      events = Devx::Schedule.coming_up.ordered
-      events = events.try(:limit, @attributes[:limit])
-      events = events.sort_by{|s| s.start_time.strftime("%H%M") }
-      events = events.try(:first, @attributes[:limit])
-      return events
+      schedules = Devx::Schedule.upcoming.ordered
+      schedules = resequence_schedules(schedules)
+      schedules = schedules.sort_by{ |s| s.start_time }
+      schedules = schedules.try(:first, @attributes[:limit])
+      return schedules
     end
 
+    # Advances all `start_time` values to the current date or the next
+    # occurrence of `days` unless the end date has passed
+    def resequence_schedules(schedules)
+      schedules.collect do |schedule|
+        current_date = Time.zone.now.to_date
+        next_occurrence = nil
+        days = schedule.days.reject(&:blank?)
+        if days.any?
+          day_indexes = days.collect{ |d| Date::DAYNAMES.index(d) }
+          current_day = current_date.strftime("%A")
+          current_day_index = Date::DAYNAMES.index(current_day)
+          until next_occurrence
+            if day_indexes.index(current_day_index)
+              next_occurrence = current_date
+            else
+              current_day_index = (current_day_index + 1 > 6) ? 0 : current_day_index + 1
+              current_date = Time.zone.now.to_date + 1.day
+            end
+          end
+        else
+          next_occurrence = current_date
+        end
+
+        if schedule.start_time.to_date < next_occurrence
+          advance = (Time.zone.now.to_date - schedule.start_time.to_date).to_i
+          schedule.start_time = schedule.start_time.advance(days: advance)
+        end
+
+        # Do not return any schedules where the next time would be past the
+        # `end_time` value.
+        (schedule.start_time < schedule.end_time) ? schedule : nil
+      end.compact
+    end
   end
 end
