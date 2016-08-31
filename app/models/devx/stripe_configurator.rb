b90e4cadbe5763d874ca59fc2fa4a082011de576
Jeffrey Lee <jlee42@gmail.com>
2016-08-31 12:09:00 -0500

    Check for presence of Stripe values before setting

--- app/models/devx/stripe_configurator.rb
+++ app/models/devx/stripe_configurator.rb
@@ -1,12 +1,17 @@
 module Devx
   class StripeConfigurator
     def initialize
-      Rails.configuration.stripe = {
-        publishable_key: Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_publishable_key'],
-        secret_key: Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_secret_key']
-      }
+      publishable_key = Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_publishable_key']
+      stripe_secret_key = Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_secret_key']
 
-      Stripe.api_key = Devx::ApplicationSetting.find_or_create_by(id: 1).settings['stripe_secret_key']
+      if publishable_key.present? && stripe_secret_key.present?
+        Rails.configuration.stripe = {
+          publishable_key: publishable_key,
+          secret_key: stripe_secret_key
+        }
+
+        Stripe.api_key = stripe_secret_key
+      end
     end
   end
 end
