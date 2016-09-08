820c883794b6c9ffa6e41803fd71f7ad939e7e14
Jeffrey Lee <jlee42@gmail.com>
2016-09-08 15:42:55 -0500

    Update split_accessors for ClassSchedule

--- app/models/devx/class_schedule.rb
+++ app/models/devx/class_schedule.rb
@@ -1,8 +1,8 @@
 module Devx
   class ClassSchedule < ActiveRecord::Base
     extend TimeSplitter::Accessors
-    split_accessor :start_time
-    split_accessor :end_time
+    split_accessor :start_time, date_format: "%m/%d/%Y", time_format: "%I:%M %P"
+    split_accessor :end_time, date_format: "%m/%d/%Y", time_format: "%I:%M %P"
 
   	belongs_to :classroom
 
