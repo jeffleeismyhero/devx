d1d48e5641dd6a57508e8372f06b845840c6da4c
Rashaad R. Randall <rashaad@jcwproductions.com>
2016-09-08 15:43:42 -0500

    Form slug regenerating

--- app/models/devx/form.rb
+++ app/models/devx/form.rb
@@ -11,5 +11,9 @@ module Devx
 
     accepts_nested_attributes_for :fields, allow_destroy: true,
       reject_if: proc{ |x| x['name'].blank? }
+
+    def should_generate_new_friendly_id?
+      name_changed?
+    end
   end
 end
