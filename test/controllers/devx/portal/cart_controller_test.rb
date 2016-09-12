ee7cb2c59e669d4823b55686a925f41147ac296d
David McGirt <david@jcwproductions.com>
2016-09-12 08:50:39 -0500

    Test Updates

---
+++ test/controllers/devx/portal/cart_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+module Devx
+  class Portal::CartControllerTest < ActionController::TestCase
+    setup do
+      @routes = Engine.routes
+    end
+
+    test "should get index" do
+      get :index
+      assert_response :success
+    end
+
+  end
+end
