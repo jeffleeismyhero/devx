4518a763d035d54f0cc1f6c83c998904fef58c3d
Rashaad R. Randall <rashaad@jcwproductions.com>
2016-09-27 15:21:45 -0500

    Fixes CKEditor bug

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -13,8 +13,7 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{id_partitioning}"
-    #"uploads/ckeditor/pictures/#{model.id}"
+    "uploads/ckeditor/pictures/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
