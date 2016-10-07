5196db79cb251e9c51221e6ac14cc8557da7381c
Jeffrey Lee <jlee42@gmail.com>
2016-10-07 16:11:12 -0500

    Create failing test for Article publishing times

--- spec/features/devx/user_creates_article_spec.rb
+++ spec/features/devx/user_creates_article_spec.rb
@@ -1,25 +1,44 @@
 require 'rails_helper'
 
 module Devx
+  Capybara.current_driver = Capybara.javascript_driver # :selenium by default
+  Capybara.current_session.driver.browser.manage.window.resize_to(1366, 768)
+
   feature 'User creates article', type: 'feature' do
-    Capybara.current_driver = Capybara.javascript_driver # :selenium by default
-    Capybara.current_session.driver.browser.manage.window.resize_to(1366, 768)
-    
-    scenario 'with all fields' do
-      skip
+    before(:each) do
       @user = create(:devx_user)
+      @article = build(:devx_article)
+      @tag = ActsAsTaggableOn::Tag.create(name: 'Test Category')
+    end
+
+    scenario 'with all required fields', js: true do
       sign_in @user
       authorize_as_super_administrator @user
 
-      expect(page).to have_content('News')
+      ensure_on devx.new_portal_article_path
+      expect(page).to have_content('Create Article')
 
-      within('.cd-side-nav') do
-        click_link_or_button('News')
+      within('form.new_article') do
+        find(:css, '#article_title').set @article.title
+        page.execute_script("$('#article_tag_list').empty().append('<option value=\"#{@tag.name}\">#{@tag.name}</option>').val('#{@tag.name}').trigger('change');")
+
+        find(:css, '#article_published_at_date').set '11/16/2016'
+        page.execute_script("$('.datepicker').datetimepicker('hide')")
+        find(:css, '#article_published_at_time').set '9:30 pm'
+        page.execute_script("$('.timepicker').timepicker('hide')")
+
+        page.execute_script("CKEDITOR.instances['article_content'].setData('Test article content')")
+
+        click_link_or_button('Create Article')
       end
-      
-      # click_link_or_button('Create Article')
 
-      # fill 'Title'
+      expect(page).to have_content('Successfully created article')
+
+      click_link_or_button(@article.title)
+
+      expect(find(:css, '#article_title').value).to eq(@article.title)
+      expect(find(:css, '#article_published_at_date').value).to eq('11/16/2016')
+      expect(find(:css, '#article_published_at_time').value).to eq('9:30 pm')
     end
   end
 end
