4e7ae36703e6ae2a508c3f8dc36ad7c29df4e9e5
Rashaad R. Randall <rashaad@jcwproductions.com>
2016-10-11 21:03:38 -0500

    rspec test fixes

--- spec/models/devx/product_sku_spec.rb
+++ spec/models/devx/product_sku_spec.rb
@@ -5,16 +5,18 @@ module Devx
     let(:product_sku){ FactoryGirl.create(:devx_product_sku) }
 
     before(:all) do
-      # Rails.configuration.stripe = {
-      #   publishable_key: 'pk_test_xBspDeWOapLw0oAh6yci2xGh',
-      #   secret_key: 'sk_test_DG3ZjPAckcGfkV3QJ5SDCCxb'
-      # }
-      # Stripe.api_key = 'sk_test_DG3ZjPAckcGfkV3QJ5SDCCxb'
-      # # Delete all associated data
-      # Stripe::Product.list.each do |stripe_product|
-      #   stripe_product.skus.each { |stripe_sku| stripe_sku.delete }
-      #   stripe_product.delete
-      # end
+      Rails.configuration.stripe = {
+        publishable_key: 'pk_test_xBspDeWOapLw0oAh6yci2xGh',
+        secret_key: 'sk_test_DG3ZjPAckcGfkV3QJ5SDCCxb'
+      }
+      Stripe.api_key = 'sk_test_DG3ZjPAckcGfkV3QJ5SDCCxb'
+      # Delete all associated data
+      Stripe::Product.list.each do |stripe_product|
+        Stripe::SKU.list(active: :false).each do |stripe_sku|
+          stripe_sku.delete
+        end
+        stripe_product.delete
+      end
     end
 
     describe 'product sku' do
@@ -24,50 +26,52 @@ module Devx
       end
     end
 
-    # describe 'stripe' do
-    #   it 'is backed by a Stripe::Product' do
-    #     expect(product.slug).to be_present
-    #     stripe_product = Stripe::Product.retrieve(product.slug)
-    #     expect(stripe_product.name).not_to be_nil
-    #   end
+    describe 'stripe' do
+      it 'is backed by a Stripe::Product' do
+        expect(product_sku.product.slug).to be_present
+        stripe_product = Stripe::Product.retrieve(product_sku.product.slug)
+        expect(stripe_product.name).not_to be_nil
+      end
 
-    #   it 'removes Stripe::Product when destroyed' do
-    #     stripe_product = Stripe::Product.retrieve(product.slug)
-    #     expect(stripe_product.name).not_to be_nil
-    #     product.destroy
-    #     expect { Stripe::Product.retrieve(product.slug) }.to raise_error Stripe::InvalidRequestError
-    #   end
+      it 'removes Stripe::Product when destroyed' do
+        stripe_product = Stripe::Product.retrieve(product_sku.product.slug)
+        expect(stripe_product.name).not_to be_nil
+        product_sku.product.destroy
+        expect { Stripe::Product.retrieve(product_sku.product.slug) }.to raise_error Stripe::InvalidRequestError
+      end
 
-    #   it 'creates Stripe::SKU records for nested Devx::Sku records' do
-    #     product = FactoryGirl.build(:devx_product)
-    #     product.product_skus.build(
-    #       currency: 'USD', price: 10.0, stockable: true, active: true
-    #     )
-    #     expect { product.save }.to change(Devx::Product, :count).by(1)
-    #                           .and change(Devx::ProductSku, :count).by(1)
+      it 'creates Stripe::SKU records for nested Devx::Sku records' do
+        product = FactoryGirl.build(:devx_product)
+        product.product_skus.build(
+          currency: 'USD', price: 10.0, stockable: true, active: true
+        )
+        expect { product.save }.to change(Devx::Product, :count).by(1)
+                              .and change(Devx::ProductSku, :count).by(1)
 
-    #     stripe_product = Stripe::Product.retrieve(product.slug)
-    #     expect(stripe_product.name).not_to be_nil
-    #     expect(stripe_product.skus.count).to eq(1)
-    #   end
+        stripe_product = Stripe::Product.retrieve(product.slug)
+        expect(stripe_product.name).not_to be_nil
+        expect(stripe_product.skus.count).to eq(1)
+      end
 
-    #   it 'removes Stripe::SKU records when destroyed' do
-    #     product = FactoryGirl.build(:devx_product)
-    #     product.product_skus.build(
-    #       currency: 'USD', price: 10.0, stockable: true, active: true
-    #     )
-    #     expect { product.save }.to change(Devx::Product, :count).by(1)
-    #                           .and change(Devx::ProductSku, :count).by(1)
+      it 'removes Stripe::SKU records when destroyed' do
+        product = FactoryGirl.build(:devx_product)
+        product.product_skus.build(
+          currency: 'USD', price: 10.0, stockable: true, active: true
+        )
+        puts product.valid?
+        puts product.errors.full_messages.each{ |e| e }
+        expect { product.save }.to change(Devx::Product, :count).by(1)
+                              .and change(Devx::ProductSku, :count).by(1)
 
-    #     stripe_product = Stripe::Product.retrieve(product.slug)
-    #     expect(stripe_product.name).not_to be_nil
-    #     expect(stripe_product.skus.count).to eq(1)
-    #     product_sku = product.product_skus.first
+        stripe_product = Stripe::Product.retrieve(product.slug)
+        expect(stripe_product.name).not_to be_nil
+        expect(stripe_product.skus.count).to eq(1)
+        product_sku = product.product_skus.first
 
-    #     product.destroy
-    #     expect { Stripe::SKU.retrieve(product_sku.stripe_id) }.to raise_error Stripe::InvalidRequestError
-    #     expect { Stripe::Product.retrieve(product.slug) }.to raise_error Stripe::InvalidRequestError
-    #   end
-    # end
+        product.destroy
+        expect { Stripe::SKU.retrieve(product_sku.stripe_id) }.to raise_error Stripe::InvalidRequestError
+        expect { Stripe::Product.retrieve(product.slug) }.to raise_error Stripe::InvalidRequestError
+      end
+    end
   end
 end
--- spec/models/devx/product_spec.rb
+++ spec/models/devx/product_spec.rb
@@ -12,7 +12,9 @@ module Devx
       Stripe.api_key = 'sk_test_DG3ZjPAckcGfkV3QJ5SDCCxb'
       # Delete all associated data
       Stripe::Product.list.each do |stripe_product|
-        stripe_product.skus.each { |stripe_sku| stripe_sku.delete }
+        Stripe::SKU.list(active: :false).each do |stripe_sku|
+          stripe_sku.delete
+        end
         stripe_product.delete
       end
     end
@@ -42,36 +44,36 @@ module Devx
         expect { Stripe::Product.retrieve(product.slug) }.to raise_error Stripe::InvalidRequestError
       end
 
-      it 'creates Stripe::SKU records for nested Devx::Sku records' do
-        product = FactoryGirl.build(:devx_product)
-        product.product_skus.build(
-          currency: 'USD', price: 10.0, stockable: true, active: true
-        )
-        expect { product.save }.to change(Devx::Product, :count).by(1)
-                              .and change(Devx::ProductSku, :count).by(1)
-
-        stripe_product = Stripe::Product.retrieve(product.slug)
-        expect(stripe_product.name).not_to be_nil
-        expect(stripe_product.skus.count).to eq(1)
-      end
-
-      it 'removes Stripe::SKU records when destroyed' do
-        product = FactoryGirl.build(:devx_product)
-        product.product_skus.build(
-          currency: 'USD', price: 10.0, stockable: true, active: true
-        )
-        expect { product.save }.to change(Devx::Product, :count).by(1)
-                              .and change(Devx::ProductSku, :count).by(1)
-
-        stripe_product = Stripe::Product.retrieve(product.slug)
-        expect(stripe_product.name).not_to be_nil
-        expect(stripe_product.skus.count).to eq(1)
-        product_sku = product.product_skus.first
-
-        product.destroy
-        expect { Stripe::SKU.retrieve(product_sku.stripe_id) }.to raise_error Stripe::InvalidRequestError
-        expect { Stripe::Product.retrieve(product.slug) }.to raise_error Stripe::InvalidRequestError
-      end
+      # it 'creates Stripe::SKU records for nested Devx::Sku records' do
+      #   product = FactoryGirl.build(:devx_product)
+      #   product.product_skus.build(
+      #     currency: 'USD', price: 10.0, stockable: true, active: true
+      #   )
+      #   expect { product.save }.to change(Devx::Product, :count).by(1)
+      #                         .and change(Devx::ProductSku, :count).by(1)
+      #
+      #   stripe_product = Stripe::Product.retrieve(product.slug)
+      #   expect(stripe_product.name).not_to be_nil
+      #   expect(stripe_product.skus.count).to eq(1)
+      # end
+      #
+      # it 'removes Stripe::SKU records when destroyed' do
+      #   product = FactoryGirl.build(:devx_product)
+      #   product.product_skus.build(
+      #     currency: 'USD', price: 10.0, stockable: true, active: true
+      #   )
+      #   expect { product.save }.to change(Devx::Product, :count).by(1)
+      #                         .and change(Devx::ProductSku, :count).by(1)
+      #
+      #   stripe_product = Stripe::Product.retrieve(product.slug)
+      #   expect(stripe_product.name).not_to be_nil
+      #   expect(stripe_product.skus.count).to eq(1)
+      #   product_sku = product.product_skus.first
+      #
+      #   product.destroy
+      #   expect { Stripe::SKU.retrieve(product_sku.stripe_id) }.to raise_error Stripe::InvalidRequestError
+      #   expect { Stripe::Product.retrieve(product.slug) }.to raise_error Stripe::InvalidRequestError
+      # end
     end
   end
 end
