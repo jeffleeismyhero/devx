3bb4cd30623cfb22a8e9185495f4e97dd2d3d991
Rashaad R. Randall <rashaad@jcwproductions.com>
2016-10-18 18:09:29 -0500

    update

--- app/mailers/devx/form_mailer.rb
+++ app/mailers/devx/form_mailer.rb
@@ -11,15 +11,19 @@ module Devx
 
       if @fields.any?
         @fields.each_with_index do |field, index|
-          sanitized_field = field.gsub(/[^a-zA-Z0-9_-]/i, '')
+          sanitized_field = field.gsub!(/[^a-zA-Z0-9_-]/i, '')
+          logger.debug "Sanitized #{sanitized_field}"
 
           logger.debug "[FORM] Checking if #{sanitized_field} is key........#{@submission.key?(sanitized_field)}"
 
           if @submission.key?(sanitized_field)
             @field = @submission[sanitized_field.to_sym]
+            if sanitized_field == 'Amount'
+              @field = number_to_currency @field
+            end
 
             logger.debug "[FORM] Replacing #{field} with #{@field}"
-            
+
             @body.gsub!(field, @field)
           end
         end
@@ -31,3 +35,6 @@ module Devx
     end
   end
 end
+
+
+Thank you for your gift to Rosarian Academy for [amount]. \n\nGiving opens precious doors of opportunity and keeps Rosarian in the forefront of teaching and learning. Whether you are supporting the day-to-day life of our students and faculty or long-term plans to keep our school viable and competitive, your gift is used carefully and invested wisely. \n\nYour Donation Details:\n\nDonation Type: [name]\n\n\nFor questions pertaining to your gift or future gifts, please contact the Advancement Office:\n561-345-3109 or advancement@rosarian.org.\n\nThank you for believing in the mission of Rosarian Academy!
