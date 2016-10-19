b8fd5e1746902e5ae5a28538baef9f96bd43890e
Rashaad R. Randall <rashaad@jcwproductions.com>
2016-10-19 15:02:11 -0500

    Fix form builder

--- app/models/devx/form.rb
+++ app/models/devx/form.rb
@@ -9,11 +9,17 @@ module Devx
     has_many :fields
     has_many :form_submissions
 
+    # before_save :update_fields
+
     accepts_nested_attributes_for :fields, allow_destroy: true,
       reject_if: proc{ |x| x['name'].blank? }
 
     def should_generate_new_friendly_id?
       name_changed?
     end
+
+    # def update_fields
+    #   self.fields.collect(&:convert_to_titlecase)
+    # end
   end
 end
--- test/mailers/previews/devx/form_mailer_preview.rb
+++ test/mailers/previews/devx/form_mailer_preview.rb
@@ -2,7 +2,7 @@ module Devx
   # Preview all emails at http://localhost:3000/rails/mailers/form_mailer
   class FormMailerPreview < ActionMailer::Preview
     def send_confirmation
-      Devx::FormMailer.send_confirmation(Devx::Form.last, Devx::FormSubmission.last.submission_content, 'info@devxcms.com')
+      Devx::FormMailer.send_confirmation(Devx::Form.find('refer-a-friend'), Devx::FormSubmission.last.submission_content, 'info@devxcms.com')
     end
   end
 end
